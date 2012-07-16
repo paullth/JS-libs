a0f76075e1a1b9cac3a6e623627c0385c943fd3b
paul <paullherve@gmail.com>
2012-07-16 09:03:08 +0100

    works ok

--- src/jquery-ui.traggable.js
+++ src/jquery-ui.traggable.js
@@ -15,118 +15,110 @@
  *  jquery.ui.mouse.js
  *  jquery.ui.widget.js
  */
-(function( $, undefined ) {
+(function($, undefined) {
 
-  $.widget("ui.traggable", $.ui.mouse, {
-    version: "1",
-    widgetEventPrefix: "trag",
-    options: {
-      addClasses: true,
-      appendTo: "parent",
-      axis: false,
-      connectToSortable: false,
-      containment: false,
-      cursor: "auto",
-      cursorAt: false,
-      grid: false,
-      handle: false,
-      helper: "original",
-      iframeFix: false,
-      opacity: false,
-      refreshPositions: false,
-      revert: false,
-      revertDuration: 500,
-      scope: "default",
-      scroll: true,
-      scrollSensitivity: 20,
-      scrollSpeed: 20,
-      snap: false,
-      snapMode: "both",
-      snapTolerance: 20,
-      stack: false,
-      zIndex: false,
-      scaledParent: false,
-      z: 1
-    },
-    _create: function() {
+    $.widget("ui.traggable", $.ui.mouse, {
+        version: "1",
+        widgetEventPrefix: "trag",
+        options: {
+            addClasses: true,
+            appendTo: "parent",
+            axis: false,
+            connectToSortable: false,
+            containment: false,
+            cursor: "auto",
+            cursorAt: false,
+            grid: false,
+            handle: false,
+            helper: "original",
+            iframeFix: false,
+            opacity: false,
+            refreshPositions: false,
+            revert: false,
+            revertDuration: 500,
+            scope: "default",
+            scroll: true,
+            scrollSensitivity: 20,
+            scrollSpeed: 20,
+            snap: false,
+            snapMode: "both",
+            snapTolerance: 20,
+            stack: false,
+            zIndex: false,
+            scaledParent: false,
+            z: 1
+        },
+        _create: function() {
+            this._mouseInit();
+            this.z = this.options.z;
+        },
 
-      
-      this._mouseInit();
+        _destroy: function() {
+            this._mouseDestroy();
+        },
 
-    },
+        _mouseCapture: function(event) {
+            var o = this.options;
+            this.eventCurrent = {
+                x: event.pageX,
+                y: event.pageY
+            };
+            return true;
+        },
 
-    _destroy: function() {
-      this._mouseDestroy();
-    },
+        _mouseStart: function(event) {
+            this.target = $(event.target).closest('.item');
+            return true;
+        },
 
-    _mouseCapture: function(event) {
+        _mouseDrag: function(event) {
+            var dX = event.pageX,
+                dY = event.pageY,
+                velX = (this.eventCurrent.x - event.pageX),
+                velY = (this.eventCurrent.y - event.pageY);
+            this._moveItem(this.target, velX, velY);
+            this.eventCurrent = {
+                x: event.pageX,
+                y: event.pageY
+            };
+            return true;
+        },
 
-      var o = this.options;
-      this.eventCurrent = {
-        x: event.pageX,
-        y: event.pageY
-      };
+        _mouseStop: function(event) {
+            return false;
+        },
 
+        _mouseUp: function(event) {
+            return $.ui.mouse.prototype._mouseUp.call(this, event);
+        },
 
-      return true;
+        _moveItem: function(target, x, y) {
+            console.log(target, x, y);
+            var position = {
+                left: parseFloat(target.css('left')),
+                top: parseFloat(target.css('top'))
+            };
+            x = x * (1 / this.z);
+            y = y * (1 / this.z);
+            target.css({
+                'left': position.left - x,
+                'top': position.top - y
+            });
+        },
+        changeScale: function(dZ){
+            console.log('changeScale', dZ, this.element);
+            this.z = this.z + dZ;
+            var scale = "scale(" + this.z + ")";
+            console.log(scale);
+            this.element.css({
+                "-webkit-transform":scale,  
+                "-moz-transform": scale,
+                "-ms-transform": scale,        
+                "-o-transform": scale,
+                "transform": scale,
+            });
+        }
 
-    },
-
-    _mouseStart: function(event) {
-      this.target = $(event.target).closest('.item');
-
-      
-      
-      return true;
-    },
-
-    _mouseDrag: function(event) {
-      this.eventCurrent = {
-        x: event.pageX,
-        y: event.pageY
-      };
-      var dX = event.pageX , dY = event.pageY, 
-      velX = (this.eventCurrent.x - event.pageX), velY = (this.eventCurrent.y - event.y);
-      this._moveItem(this.target, velX, velY);
-      return false;
-    },
-
-    _mouseStop: function(event) {
-      
-
-      return false;
-    },
-    
-    _mouseUp: function(event) {
-      
-      
-      //If the ddmanager is used for droppables, inform the manager that dragging has stopped (see #5003)
-      if( $.ui.ddmanager ) $.ui.ddmanager.dragStop(this, event);
-      
-      return $.ui.mouse.prototype._mouseUp.call(this, event);
-    },
-
-    _moveItem: function(target, x, y){
-      console.log(target, x, y);
-      var position = {
-        left: parseInt(target.css('left')),
-        top: parseInt(target.css('top'))
-      };
-      target.css({
-        'left': position.left + x,
-        'top': position.top + y
-      });
-    }
-
-  });
-
-})(jQuery);
-
-(function($, undefined){
-    $(document).ready(function(){
-        console.log('ready');
-        $('.items').traggable();
     });
+
 })(jQuery);
-    
-​
